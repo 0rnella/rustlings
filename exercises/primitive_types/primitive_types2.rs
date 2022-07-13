1c14f0fff5cfbbafab1a70b1c994591852cfa79b
friggito <ornella.friggit@gmail.com>
2022-07-13 09:27:30 +0200

    enums

--- exercises/primitive_types/primitive_types2.rs
+++ exercises/primitive_types/primitive_types2.rs
@@ -1,8 +1,7 @@
 // primitive_types2.rs
 // Fill in the rest of the line that has code missing!
 // No hints, there's no tricks, just get used to typing these :)
-
-// I AM NOT DONE
+ 
 
 fn main() {
     // Characters (`char`)
